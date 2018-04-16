a63c50f353cd7125b7eef8fb20fcd37956c5c2dd
Fabrizio Stellato <fabrizio.stellato@infocert.it>
2018-04-16 16:26:41 +0200

    improved build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-msbuild /t:rebuild /p:Configuration=Release /p:DebugSymbols=false /p:DebugType=None /p:Platform="x64" /p:OutputPath=<output_path>
+set output_path=D:\SMS_Software\xrns2xmod
+
+msbuild /t:rebuild /p:Configuration=Release /p:DebugSymbols=false /p:DebugType=None /p:Platform="x64" /p:OutputPath=%output_path%
