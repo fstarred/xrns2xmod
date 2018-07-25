74916d9d77c2c3b419454b3436ec628fb08ab7ac
Fabrizio Stellato <fabrizio.stellato@infocert.it>
2018-07-25 12:51:22 +0200

    added a shared assembly to unify version accross multiple project

--- Xrns2XMod/Properties/AssemblyInfo.cs
+++ Xrns2XMod/Properties/AssemblyInfo.cs
@@ -1,36 +1,20 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+
+// Note: Shared assembly information is specified in SharedAssemblyInfo.cs
+
+// General Information about an assembly is controlled through the following
+
 // set of attributes. Change these attribute values to modify the information
+
 // associated with an assembly.
-[assembly: AssemblyTitle("Xrns2XMod dll")]
-[assembly: AssemblyDescription("A tool for convert from renoise song format to mod / xm")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Zenon")]
-[assembly: AssemblyProduct("Xrns2XMod")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
-[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyTitle("Xrns2XMod core library")]
+
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3440645d-cef8-488f-88b3-19bcb99cdc0a")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.*")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: Guid("3440645d-cef8-488f-88b3-19bcb99cdc0a")]
