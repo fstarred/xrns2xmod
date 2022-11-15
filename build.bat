1ada299b155b1e9b26c1cebf9719526f8d77a000
Fabrizio Stellato <fabrizio.stellato@cgm.com>
2022-11-15 15:44:11 +0100

    updated version and added bass libraries

--- build.bat
+++ build.bat
@@ -14,6 +14,8 @@ msbuild Xrns2XModCmd/Xrns2XModCmd.csproj /t:rebuild /p:Configuration=Release /p:
 
 msbuild Xrns2XModUnitTest\Xrns2XModUnitTest.csproj /t:rebuild /p:Configuration=Release /p:DebugSymbols=false /p:DebugType=None /p:Platform="x64" /p:OutputPath=%output_test_path%
 
+exit /B
+
 :no_command_found
 echo This batch must be launched from Developer Command Prompt for VS 2xxx
 exit /B 1
